72a278a1f0a37f1b490998ccf24ff2e973de5fda
MinghuiZhao39 <mzhao39@sheffield.ac.uk>
2024-07-08 15:36:29 +0100

    update

--- Grad-TTS/model/monotonic_align/setup.py
+++ Grad-TTS/model/monotonic_align/setup.py
@@ -1,11 +1,17 @@
 """ from https://github.com/jaywalnut310/glow-tts """
 
-from distutils.core import setup
+# from distutils.core import setup
+# from Cython.Build import cythonize
+# import numpy
+
+# setup(
+#     name = 'monotonic_align',
+#     ext_modules = cythonize("core.pyx"),
+#     include_dirs=[numpy.get_include()]
+# )
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-setup(
-    name = 'monotonic_align',
-    ext_modules = cythonize("core.pyx"),
-    include_dirs=[numpy.get_include()]
-)
+package = Extension('core', ['core.pyx'], include_dirs=[numpy.get_include()])
+setup(ext_modules=cythonize([package]))
