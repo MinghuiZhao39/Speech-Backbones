85ba0e5d594e3f030e377f244a184bbcc3635384
MinghuiZhao39 <mzhao39@sheffield.ac.uk>
2024-11-04 22:17:43 +0000

    made it causal

--- Grad-TTS/model/attention.py
+++ Grad-TTS/model/attention.py
@@ -2,6 +2,48 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# class ResidualConnection(nn.Module):
+
+#     def __init__(self, features: int, dropout: float) -> None:
+#         super().__init__()
+#         self.dropout = nn.Dropout(dropout)
+#         self.norm = LayerNormalization(features)
+
+#     def forward(self, x, sublayer):
+#         return x + self.dropout(sublayer(self.norm(x)))
+
+# class LayerNormalization(nn.Module):
+
+#     def __init__(self, features: int, eps: float = 10**-6) -> None:
+#         super().__init__()
+#         self.eps = eps
+#         self.alpha = nn.Parameter(
+#             torch.ones(features)
+#         )  # alpha is a learnable parameter
+#         self.bias = nn.Parameter(torch.zeros(features))  # bias is a learnable parameter
+
+#     def forward(self, x):
+#         # x: (batch, seq_len, hidden_size)
+#         # Keep the dimension for broadcasting
+#         mean = x.mean(dim=-1, keepdim=True)  # (batch, seq_len, 1)
+#         # Keep the dimension for broadcasting
+#         std = x.std(dim=-1, keepdim=True)  # (batch, seq_len, 1)
+#         # eps is to prevent dividing by zero or when std is very small
+#         return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+# class FeedForwardBlock(nn.Module):
+
+#     def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+#         super().__init__()
+#         self.linear_1 = nn.Linear(d_model, d_ff)  # w1 and b1
+#         self.dropout = nn.Dropout(dropout)
+#         self.linear_2 = nn.Linear(d_ff, d_model)  # w2 and b2
+
+#     def forward(self, x):
+#         # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
+#         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+
 class Attention(nn.Module):
     def __init__(self, input_dim, hidden_dim=512):
         super(Attention, self).__init__()
@@ -13,7 +55,8 @@ class Attention(nn.Module):
         self.fc2 = nn.Linear(hidden_dim, input_dim)
         
         # Attention mechanism
-        self.attention = nn.MultiheadAttention(embed_dim=hidden_dim, num_heads=8, batch_first=True)
+        self.self_attention = nn.MultiheadAttention(embed_dim=hidden_dim, num_heads=8, batch_first=True)
+        self.cross_attention = nn.MultiheadAttention(embed_dim=hidden_dim, num_heads=8, batch_first=True)
         
     def forward(self, query, key, value, mask):
         # Transform the input dimensions to hidden_dim
@@ -22,7 +65,8 @@ class Attention(nn.Module):
         value = self.fc1(value)
         
         # Apply attention
-        attn_output, _ = self.attention(query, key, value, key_padding_mask=mask)
+        query = self.self_attention(query, query, query, key_padding_mask=mask, is_causal=True)[0]
+        attn_output = self.cross_attention(query, key, value, key_padding_mask=mask)[0]
         
         output = self.fc2(attn_output)
         
